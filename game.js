324c5b66c784a285457065a358d151cf22df580f
betermux <betermux@gmail.com>
2025-08-10 22:35:38 +0800

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,28 @@
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
 
+let bird = { x: 100, y: 300, velocity: 0, gravity: 0.5, jump: -10 };
+let birdImg = new Image();
+birdImg.src = 'assets/bird.png';
+
+let score = 0;
+
+function drawBird() {
+    ctx.drawImage(birdImg, bird.x, bird.y, 30, 30);
+}
+
+function update() {
+    bird.velocity += bird.gravity;
+    bird.y += bird.velocity;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBird();
+    requestAnimationFrame(update);
+}
+
+canvas.addEventListener('click', () => {
+    bird.velocity = bird.jump;
+});
+
+birdImg.onload = () => {
+    update();
+};
