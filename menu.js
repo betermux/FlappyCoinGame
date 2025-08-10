0d8fa0707197567c7ab56b65192d0622ad648ab2
betermux <betermux@gmail.com>
2025-08-10 22:35:53 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,24 @@
+function startGame() {
+    console.log("Game started!");
+    // Тоглоомын логик эндээс дуудагдана
+}
 
+function openSkins() {
+    console.log("Skins menu opened!");
+    // Skin сонгох цонхыг нээх
+}
+
+function showLeaderboard() {
+    console.log("Leaderboard opened!");
+    // Онооны жагсаалтыг харуулах
+}
+
+function openShop() {
+    console.log("Shop opened!");
+    // Дэлгүүр нээх
+}
+
+function openUpgrade() {
+    console.log("Upgrade menu opened!");
+    // Шинэчлэлийн цонхыг нээх
+}
