8571b29455df511d75d031213b1521c07b9e132e
betermux <betermux@gmail.com>
2025-08-10 23:04:13 +0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,24 +1,29 @@
 function startGame() {
     console.log("Game started!");
-    // Тоглоомын логик эндээс дуудагдана
+    window.Telegram.WebApp.showAlert("Тоглоом эхэллээ!"); // Telegram alert
+    // Тоглоомын логик эндээс эхэлнэ
 }
 
 function openSkins() {
     console.log("Skins menu opened!");
-    // Skin сонгох цонхыг нээх
+    window.Telegram.WebApp.showPopup({
+        title: "Skins",
+        message: "Шинэ skin сонгох уу?",
+        buttons: [{ type: "ok" }, { type: "cancel" }]
+    });
 }
 
 function showLeaderboard() {
     console.log("Leaderboard opened!");
-    // Онооны жагсаалтыг харуулах
+    window.Telegram.WebApp.showAlert("Онооны жагсаалт удахгүй нэмэгдэнэ!");
 }
 
 function openShop() {
     console.log("Shop opened!");
-    // Дэлгүүр нээх
+    window.Telegram.WebApp.showAlert("Дэлгүүр удахгүй нэмэгдэнэ!");
 }
 
 function openUpgrade() {
     console.log("Upgrade menu opened!");
-    // Шинэчлэлийн цонхыг нээх
+    window.Telegram.WebApp.showAlert("Шинэчлэл удахгүй нэмэгдэнэ!");
 }
